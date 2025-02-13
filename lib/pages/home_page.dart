42f23ea65020f575c093cfa958e6063a7bc170e3
Eduardo450 <LE23080483@merida.tecnm.mx>
2025-02-13 13:32:29 -0600

    Main Text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,8 +14,40 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         //Icono de la izquierda
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
-        actions: [Icon(Icons.person)]
+        leading: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Icon(Icons.menu, color: Colors.grey[800]),
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Icon(Icons.person),
+        )]
+      ),
+      body: Column(
+        children: <Widget>[
+          //1. Texto Principal (Text)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          child: Row(
+            children: [
+              Text("I want to ", style: TextStyle(
+                fontSize: 20
+              )),
+              Text("Eat...", style: TextStyle(
+                //Tamaño de letra
+                fontSize: 32,
+                //Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline,
+              ),),
+            ],
+          ),
+        )
+        //2. Pestañas (TabBar)
+        //3. Contenido de Pestañas (TabBarView)
+        //4. Carrito (Cart)
+        ],
       ),
     );
   }
