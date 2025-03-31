4a80481e91248a61abd44446e7c72ae6c83750fa
Eduardo450 <LE23080483@merida.tecnm.mx>
2025-03-31 12:44:43 -0600

    DonutTile Finished (Class Design)

--- lib/pages/utils/donut_tile.dart
+++ lib/pages/utils/donut_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
+  final String donutStore;
   final String donutPrice;
     //dynamic por que será de tipo color
   final dynamic donutColor;
@@ -10,6 +11,7 @@ class DonutTile extends StatelessWidget {
   const DonutTile(
     {super.key,
     required this.donutFlavor,
+    required this.donutStore,
     required this.donutPrice,
     required this.donutColor,
     required this.imageName}
@@ -51,63 +53,58 @@ class DonutTile extends StatelessWidget {
                 ),
               ],
             ),
+
             //Donut price
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
               child: Image.asset(imageName),
             ),
-            //Donut Flavor
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                  child:Text(
-                    "$donutFlavor",
+
+            //Donut Flavor Text
+            Text(
+                    donutFlavor,
                     style:TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 18,
                       color: Colors.black87,
                     )
                   ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                  child:Text(
-                    "Dunkin's",
+            Text(
+                    donutStore,
                     style:TextStyle(
                       fontSize: 12,
                       color: Colors.black54,
                     )
                   ),
-            ),
-            //Icons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                    child:Icon(Icons.favorite_border_outlined,
-                      color: Colors.grey[800])
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                  child: TextButton(
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.grey[800],
+                  ),
+                  TextButton(
                     style: TextButton.styleFrom(
                       padding: EdgeInsets.zero, // Elimina el padding interno del botón
                       minimumSize: Size(29, 29), // Reduce el tamaño mínimo del botón
                       tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Reduce el área táctil
                     ),
-                    onPressed: (){},
+                    onPressed: () {},
                     child: Text(
-                        "Add",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline,
-                          color: Colors.black87,
-                        ),
+                      "Add",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                        color: Colors.black87,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
